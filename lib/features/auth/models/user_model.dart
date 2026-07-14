79f94dbb22ace917c57a76eb7fe9f395caa9acfb
ArchNemesis456 <swayamss0456@gmail.com>
2026-07-14 11:06:05 +0530

    feat: add user model

---
+++ lib/features/auth/models/user_model.dart
@@ -0,0 +1,45 @@
+class UserModel {
+  final String uid;
+  final String? name;
+  final String email;
+  final String? photoUrl;
+
+  const UserModel({
+    required this.uid,
+    this.name,
+    required this.email,
+    this.photoUrl,
+  });
+
+  UserModel copyWith({
+    String? uid,
+    String? name,
+    String? email,
+    String? photoUrl,
+  }) {
+    return UserModel(
+      uid: uid ?? this.uid,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      photoUrl: photoUrl ?? this.photoUrl,
+    );
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'] as String,
+      name: map['name'] as String?,
+      email: map['email'] as String,
+      photoUrl: map['photoUrl'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'name': name,
+      'email': email,
+      'photoUrl': photoUrl,
+    };
+  }
+}
